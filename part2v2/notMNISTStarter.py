f19d56eb6f2310767d6836632b63de5edca67787
stefan.jovanovic <stefan.jovanovic@zone42-ed.cs.ucalgary.ca>
2019-12-05 20:16:41 -0700

    finished part2

--- part2v2/notMNISTStarter.py
+++ part2v2/notMNISTStarter.py
@@ -18,6 +18,13 @@ print(len(y_train))
 x_train, x_test = x_train / 255.0, x_test / 255.0
  
 print("--Make model--")
+initial_learning_rate = 0.1
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate,
+    decay_steps=100000,
+    decay_rate=0.96,
+    staircase=True)
+
 model = tf.keras.models.Sequential([
 
   #tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -28,7 +35,9 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dropout(0.1),
   tf.keras.layers.Dense(10, activation='softmax')
 ])
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr_schedule),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
 print("--Fit model--")
 model.fit(x_train, y_train, epochs=15, verbose=2)
